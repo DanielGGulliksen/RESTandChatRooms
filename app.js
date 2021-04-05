2288538d46fa4462b2e0db8aece76def090cb6a5
h588512 <54103587+h588512@users.noreply.github.com>
2021-04-05 19:50:17 +0200

    All operations for '/api/users' implemented

--- app.js
+++ app.js
@@ -16,25 +16,24 @@ let users = {}
  *  the provided URI by appending '?id=1' or '?id=2', for example. 
  */
 app.get('/api/users', (req, res) => {
-    let id = req.query.id;
+    let id = req.query.id;        // Is there a better way to get a specific object?
     if (id == undefined) { // Tests if 'id' was provided
-        res.send(users)
+        res.send(users)    // If 'id' not provided, returns the entire dictionary. (Get all)
     }
     else {
-    let len= Object.keys(users).length;
-    if (id >= len)
-        res.send("The provided 'id' is out of bounds.");
-    else
-        if (len > 0) {
-            let found = false;
-            for (let i = 0; i < len && !found; i++) {
-                if (i = id) {
-                    res.send(users[i]);
-                    found = true;
-                }
-            }
-        }    
-        else res.send(users);
+        let len= Object.keys(users).length;
+        if (len == 0)
+            res.send("The stored dictionary is empty.");
+        else
+            if (id >= len)
+                res.send("The provided 'id' is out of bounds.");
+            else {
+                const dict = users[id]; 
+                if (dict == undefined)
+                    res.send("No object with ID '" + id + "' was found.");
+                else
+                    res.send(dict)
+            }                        
     }
 });
 
@@ -60,6 +59,26 @@ app.post('/api/users', (req, res) => {
     else {
         users[0] = req.body;
         res.send(res.statusCode + ": Successfully added " + users[0].name + " at index: 0");
+    }
+});
+
+app.delete('/api/users', (req, res) => {
+    console.log(Object.keys(users).length);
+    let id = req.query.id;             // Is there a better way to delete a specific object?
+    if (id == undefined) { // Tests if 'id' was provided
+        res.send("The provided ID was undefined.")
+    }
+    else {
+        let len= Object.keys(users).length;
+        if (len == 0)
+            res.send("The stored dictionary is empty.");
+        else
+            if (users[id] == undefined)
+                res.send("No object with ID '" + id + "' was found.");
+            else {
+                users[id] = undefined;
+                res.send(res.statusCode + ": Object with ID '" + id + "' was successfully deleted");
+            }
     }
 });
 
