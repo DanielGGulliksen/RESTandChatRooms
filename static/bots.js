515c8cf5de2f7fea4c97b80e9548ba3d4375d911
h588512 <54103587+h588512@users.noreply.github.com>
2021-04-13 18:05:28 +0200

    all bots join on get

--- static/bots.js
+++ static/bots.js
@@ -1,3 +1,24 @@
-function hehe(){
-    document.getElementById("two").innerHTML = "Winston is now running...";
+function launch() {
+    
+    document.getElementById("one").innerHTML = json.name + " is now running...";
+
+    document.getElementById("launcher").remove();
+    url = "http://localhost:5000"
+    
+    // Add self to users dictionary     
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url + "/api/users", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+      
+    xhr.onload  = function() {
+        var res = xhr.response;
+        $("#response").text(res);
+    };
+    xhr.send(JSON.stringify(json));
+
+    switch (json) {
+        case "Winston":
+            
+        break;
+    }
 }
